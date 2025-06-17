f37b27cf1bb7fc5ead73159c2ebeaf70fc1d85ed
beatrizfppo <b.cirino@aluno.senai.br>
2025-06-17 11:29:36 -0300

    oi

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
- 
- 
+const form = document.getElementById('form');
+const nome = document.getElementById('nome');
+const peso = document.getElementById('peso');
+const altura = document.getElementById('altura');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function (event){
+    event.preventDefault();
+    calcIMC(peso, altura);
+})
+
+function calcIMC(p, a){
+    const conta = p/(a*a);
+    if (conta > 40){
+        const imc = document.createElement('p')
+    }
+}
